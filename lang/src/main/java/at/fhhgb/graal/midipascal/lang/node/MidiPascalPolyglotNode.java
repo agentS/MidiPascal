efca25a63b140feafd3ad361b6b2f575100e0cf2
Lukas Schoerghuber <lukas.schoerghuber@posteo.at>
2018-10-14 18:14:25 +0200

    Add annotations to the parameters of the polyglot node

--- lang/src/main/java/at/fhhgb/graal/midipascal/lang/node/MidiPascalPolyglotNode.java
+++ lang/src/main/java/at/fhhgb/graal/midipascal/lang/node/MidiPascalPolyglotNode.java
@@ -1,11 +1,15 @@
 package at.fhhgb.graal.midipascal.lang.node;
 
+import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import org.graalvm.polyglot.Context;
 
 public class MidiPascalPolyglotNode extends MidiPascalStatementNode
 {
+	@CompilerDirectives.CompilationFinal
 	private String targetLanguage;
+
+	@Child
 	private MidiPascalExpressionNode parameter;
 
 	public MidiPascalPolyglotNode(String targetLanguage, MidiPascalExpressionNode parameter)
